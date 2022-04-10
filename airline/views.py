4a5c2c34e4452b79cacc7c45b027c0e7c0f5bee2
abamaj <abamaj@gmail.com>
2022-04-10 17:10:40 -0600

    first rendering

--- airline/views.py
+++ airline/views.py
@@ -2,13 +2,13 @@
 
 from django.shortcuts import render
 from django.http import HttpResponse
-from requests import request
+
 """Views page for the airline application."""
 
 
 def home(request):
     """Home page function."""
-    return render(request, 'airline/main.html')
+    return render(request, 'airline/home.html')
 
 
 def about():
